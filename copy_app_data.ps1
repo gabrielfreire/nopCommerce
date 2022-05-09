051afcca17ece58d6fa0362a3a5b85a21556186b
Gabriel Freire <gabrielfreiredev@gmail.com>
2022-05-09 09:39:06 +0100

    fix script

--- copy_app_data.ps1
+++ copy_app_data.ps1
@@ -3,6 +3,6 @@ $_pass = "kubeadmin"
 $_host = "192.168.0.214"
 
 $_linuxPath = "/home/ubuntu/data/nopCommerce/App_Data"
-scp -r .\src\Presentation\Nop.Web\App_Data\* $($_user)@$($_host):$($_linuxPath)
+scp -r .\src\Presentation\Nop.Web\App_Data\* "$_user@$($_host):$_linuxPath"
 
 ~/SshScript.exe -h $_host -u $_user -p $_pass exec -c "chown -R root:root /home/ubuntu/data/nopCommerce/*"
